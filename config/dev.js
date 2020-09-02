e6eddc96ffa6d3e4b0bdbbc5fff3b7ec56f0fd31
mhshuvoit <mhshuvoit@gmail.com>
2020-09-02 20:02:49 +0600

    add

--- config/dev.js
+++ config/dev.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoURI: 'mongodb://localhost:27017/msdp'
+    // mongoURI: 'mongodb://localhost:27017/msdp'
+    mongoURI: 'mongodb+srv://mhshuvoit:mhshuvoit@cluster1.xbs5i.mongodb.net/msdp?retryWrites=true&w=majority'
 }
