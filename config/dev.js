f18b4eb1f4dc0ff66b900cb8bb425bfda60b9617
mhshuvoit <mhshuvoit@gmail.com>
2020-09-02 19:48:20 +0600

    config

--- config/dev.js
+++ config/dev.js
@@ -1,4 +1,3 @@
 module.exports = {
-    // mongoURI: 'mongodb://localhost:27017/msdp'
-    mongoURI: 'mongodb+srv://mhshuvoit:mhshuvoit@cluster1.xbs5i.mongodb.net/msdp?retryWrites=true&w=majority'
+    mongoURI: 'mongodb://localhost:27017/msdp'
 }
--- config/prod.js
+++ config/prod.js
@@ -1,4 +1,3 @@
 module.exports = {
-    // mongoURI: process.env.MONGO_URI
-    mongoURI: 'mongodb+srv://mhshuvoit:mhshuvoit@cluster1.xbs5i.mongodb.net/msdp?retryWrites=true&w=majority'
+    mongoURI: process.env.MONGO_URI
 }
